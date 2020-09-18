1e679f0483fd5ed714e942f575a0c73798f98fae
Daniel Cazzulino <daniel@cazzulino.com>
2020-09-18 08:20:25 -0300

    Code was too tight and harder to read without braces

--- src/GeneratorExtension.cs
+++ src/GeneratorExtension.cs
@@ -34,11 +34,15 @@ namespace ThisAssembly
             if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.DebugSourceGenerators", out var debugValue) &&
                 bool.TryParse(debugValue, out var shouldDebug) &&
                 shouldDebug)
+            {
                 Debugger.Launch();
+            }
             else if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.Debug" + generatorName, out debugValue) &&
                 bool.TryParse(debugValue, out shouldDebug) &&
                 shouldDebug)
+            {
                 Debugger.Launch();
+            }
         }
     }
 }
