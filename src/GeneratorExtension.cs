c4ac95e28e0ce3c26de5a2af66ac38cedc580ead
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-22 23:36:16 -0300

    Add ThisAssembly.Constants to generate arbitrary constants from MSBuild
    Add support for both Constant and FileConstant items:
    
    ```xml
      <ItemGroup>
        <Constant Include="Foo.Bar" Value="Baz" Comment="Yay!" />
        <FileConstant Include="@(None)" />
      <ItemGroup>
    ```

--- src/GeneratorExtension.cs
+++ src/GeneratorExtension.cs
@@ -4,45 +4,42 @@ using System.IO;
 using System.Text;
 using Microsoft.CodeAnalysis;
 
-namespace ThisAssembly
+static class GeneratorExtensions
 {
-    static class GeneratorExtensions
+    public static void ApplyDesignTimeFix(this GeneratorExecutionContext context, string content, string hintName, string language)
     {
-        public static void ApplyDesignTimeFix(this GeneratorExecutionContext context, string content, string hintName, string language)
+        var includeFix = context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.IncludeSourceGeneratorIntellisenseFix", out var raw) &&
+            bool.TryParse(raw, out var value) &&
+            value;
+
+        if (includeFix)
         {
-            var includeFix = context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.IncludeSourceGeneratorIntellisenseFix", out var raw) &&
-                bool.TryParse(raw, out var value) &&
-                value;
+            if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.ThisAssemblyIntellisenseFixExtension", out var extension))
+                extension = ".ta.g." + (language == LanguageNames.CSharp ? "cs" : language == LanguageNames.VisualBasic ? "vb" : "fs");
 
-            if (includeFix)
-            {
-                if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.ThisAssemblyIntellisenseFixExtension", out var extension))
-                    extension = ".ta.g." + (language == LanguageNames.CSharp ? "cs" : language == LanguageNames.VisualBasic ? "vb" : "fs");
+            if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.IntermediateOutputPath", out var intermediate))
+                throw new NotSupportedException();
 
-                if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.IntermediateOutputPath", out var intermediate))
-                    throw new NotSupportedException();
-
-                var path = Path.Combine(intermediate, hintName + extension);
-                Directory.CreateDirectory(intermediate);
-                File.WriteAllText(path, content, Encoding.UTF8);
-            }
+            var path = Path.Combine(intermediate, hintName + extension);
+            Directory.CreateDirectory(intermediate);
+            File.WriteAllText(path, content, Encoding.UTF8);
         }
+    }
 
-        //[Conditional("DEBUG")]
-        public static void CheckDebugger(this GeneratorExecutionContext context, string generatorName)
+    //[Conditional("DEBUG")]
+    public static void CheckDebugger(this GeneratorExecutionContext context, string generatorName)
+    {
+        if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.DebugSourceGenerators", out var debugValue) &&
+            bool.TryParse(debugValue, out var shouldDebug) &&
+            shouldDebug)
         {
-            if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.DebugSourceGenerators", out var debugValue) &&
-                bool.TryParse(debugValue, out var shouldDebug) &&
-                shouldDebug)
-            {
-                Debugger.Launch();
-            }
-            else if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.Debug" + generatorName, out debugValue) &&
-                bool.TryParse(debugValue, out shouldDebug) &&
-                shouldDebug)
-            {
-                Debugger.Launch();
-            }
+            Debugger.Launch();
+        }
+        else if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.Debug" + generatorName, out debugValue) &&
+            bool.TryParse(debugValue, out shouldDebug) &&
+            shouldDebug)
+        {
+            Debugger.Launch();
         }
     }
 }
---
+++ src/ThisAssembly.Tests/ConstantsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisAssemblyTests
+{
+    class ConstantsTests
+    {
+        public ConstantsTests()
+        {
+
+
+
+
+
+        }
+    }
+}
