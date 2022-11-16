99903ea8e64de064d91cefa85f3e2b25bf5f81ab
Stuart Turner <stuart@turner-isageek.com>
2022-11-15 20:04:10 -0600

    Cleanup

--- src/ThisAssembly.Resources/ResourcesGenerator.cs
+++ src/ThisAssembly.Resources/ResourcesGenerator.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Xml.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 using Scriban;
