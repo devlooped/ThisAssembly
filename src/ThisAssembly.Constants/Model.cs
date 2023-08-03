4f2072884524c59f5cd75d1df4fad335b207a7e6
fabien.menager <fabien.menager@repply.fr>
2023-08-03 21:16:25 +0200

    Escape invalid identifiers for file constants

--- src/ThisAssembly.Constants/Model.cs
+++ src/ThisAssembly.Constants/Model.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis.CSharp;
 
 [DebuggerDisplay("Values = {RootArea.Values.Count}")]
 record Model(Area RootArea)
@@ -16,6 +18,27 @@ record Area(string Name, string Prefix)
 {
     public List<Area> NestedAreas { get; init; } = new();
     public List<Constant> Values { get; init; } = new();
+    
+    private static string EscapeIdentifier(string identifier)
+    {
+        if (string.IsNullOrWhiteSpace(identifier))
+        {
+            return "_";
+        }
+        
+        var replaced = identifier
+            .Select(c => SyntaxFacts.IsIdentifierPartCharacter(c) ? c : '_')
+            .ToArray();
+
+        var result = Regex.Replace(new string(replaced), "(_)+", "_");
+
+        if (!SyntaxFacts.IsIdentifierStartCharacter(result[0]))
+        {
+            result = "_" + result;
+        }  
+
+        return result;
+    }
 
     public static Area Load(List<Constant> constants, string rootArea = "Constants")
     {
@@ -24,15 +47,18 @@ record Area(string Name, string Prefix)
         foreach (var constant in constants)
         {
             //  Splits: ([area].)*[name]
-            var parts = constant.Name.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries);
+            var parts = constant.Name.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(EscapeIdentifier)
+                .ToArray();
+            
             if (parts.Length <= 1)
             {
-                root.Values.Add(new Constant(constant.Name, constant.Value, constant.Comment));
+                root.Values.Add(constant with { Name = EscapeIdentifier(constant.Name) });
             }
             else
             {
                 var area = GetArea(root, parts.Take(parts.Length - 1));
-                var value = new Constant(parts.Skip(parts.Length - 1).First(), constant.Value, constant.Comment);
+                var value = constant with { Name = parts.Skip(parts.Length - 1).First() };
 
                 area.Values.Add(value);
             }
